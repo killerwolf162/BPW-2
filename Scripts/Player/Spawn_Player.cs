a5b71dfd5af6e78051043d62c6bdab7c9c2ad898
Main_PC_Tom <tjongman.123@gmail.com>
2024-02-07 16:18:30 +0100

    Setup now works yes?

---
+++ Scripts/Player/Spawn_Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Player : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject Start_location;
+
+
+    void Start()
+    {
+        Start_location = GameObject.FindGameObjectWithTag("Floor");
+        Spawn_player();
+    }
+
+    void Spawn_player()
+    {
+        Instantiate(Player, Start_location.transform.position + new Vector3(2, 0), Start_location.transform.rotation);
+    }
+
+}
