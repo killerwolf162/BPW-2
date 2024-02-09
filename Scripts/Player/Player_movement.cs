2ca6ffd71e34db133b80a7b865606a990d3cabfb
Main_PC_Tom <tjongman.123@gmail.com>
2024-02-09 13:23:24 +0100

    Animating player_char

--- Scripts/Player/Player_movement.cs
+++ Scripts/Player/Player_movement.cs
@@ -7,11 +7,13 @@ public class Player_movement : MonoBehaviour
 {
 
     private Rigidbody rig;
+    private Animator animator;
     public float speed = 1f;
 
     private void Awake()
     {
         rig = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -20,6 +22,11 @@ public class Player_movement : MonoBehaviour
         Vector3 m_input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         rig.MovePosition(transform.position + m_input * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            animator.SetBool("IsWalking", true);
+        else
+            animator.SetBool("IsWalking", false);
     }
 
 
