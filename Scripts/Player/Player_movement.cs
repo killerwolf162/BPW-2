401997dd4d8eceabb6b762c8aa68d1c987c97999
Main_PC_Tom <tjongman.123@gmail.com>
2024-02-09 23:42:14 +0100

    no message

--- Scripts/Player/Player_movement.cs
+++ Scripts/Player/Player_movement.cs
@@ -20,19 +20,13 @@ public class Player_movement : MonoBehaviour
     private void Update()
     {
 
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.CompareTag("Wall"))
-                Debug.Log("collided");
-        }
-
         if (Input.GetKeyDown(KeyCode.D))
             move_right();
-        if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A))
             move_left();
-        if (Input.GetKeyDown(KeyCode.W))
+        else if (Input.GetKeyDown(KeyCode.W))
             move_forward();
-        if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S))
             move_backward();
 
         /*if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0 || Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
@@ -45,46 +39,35 @@ public class Player_movement : MonoBehaviour
     public void move_right()
     {
         rig.MovePosition(transform.position + new Vector3(1, 0, 0));
-        void OnTriggerEnter(Collider other)
-        {
-
-            //if (other.gameObject.CompareTag("Wall"))
-              //  rig.MovePosition(transform.position + new Vector3(-1, 0, 0));
-        }
-
     }
 
     public void move_left()
     {
         rig.MovePosition(transform.position + new Vector3(-1, 0, 0));
-
-        void OnTriggerEnter(Collider other)
-        {
-           // if (other.gameObject.CompareTag("Wall"))
-               // rig.MovePosition(transform.position + new Vector3(1, 0, 0));
-        }
     }
 
     public void move_forward()
     {
         rig.MovePosition(transform.position + new Vector3(0, 0, 1));
-
-        void OnTriggerEnter(Collider other)
-        {
-           // if (other.gameObject.CompareTag("Wall"))
-              //  rig.MovePosition(transform.position + new Vector3(0, 0, -1));
-        }
     }
 
     public void move_backward()
     {
         rig.MovePosition(transform.position + new Vector3(0, 0, -1));
+    }
 
-        void OnTriggerEnter(Collider other)
-        {
-           // if (other.gameObject.CompareTag("Wall"))
-             //   rig.MovePosition(transform.position + new Vector3(0, 0, 1));
-        }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Wall") && Input.GetKey(KeyCode.D))
+            rig.MovePosition(transform.position + new Vector3(-1, 0, 0));
+        else if (other.gameObject.CompareTag("Wall") && Input.GetKey(KeyCode.A))
+            rig.MovePosition(transform.position + new Vector3(1, 0, 0));
+        else if (other.gameObject.CompareTag("Wall") && Input.GetKey(KeyCode.W))
+            rig.MovePosition(transform.position + new Vector3(0, 0, -1));
+        else if (other.gameObject.CompareTag("Wall") && Input.GetKey(KeyCode.S))
+            rig.MovePosition(transform.position + new Vector3(0, 0, 1));
+        
     }
 
 }
